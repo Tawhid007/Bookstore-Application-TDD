64f502740916837f37efcc91a32dab91ee82fde7
tawhid <tawhid@nascenia.com>
2016-03-01 11:34:35 +0600

    Add seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Author.create!(first_name: 'Agatha', last_name: 'Christie')
+Author.create!(first_name: 'Rabindranath', last_name: 'Tagore')
+Author.create!(first_name: 'Nazrul', last_name: 'Islam')
+Author.create!(first_name: 'Shamsul', last_name: 'Haque')
+Author.create!(first_name: 'Jibonanondo', last_name: 'Dash')
+Author.create!(first_name: 'Mujtoba', last_name: 'Ali')
+
+Publisher.create!([
+                      {name: 'Anondo'},
+                      {name: 'Ononna'},
+                      {name: 'Shaikat'},
+                      {name: 'Akash'},
+                      {name: 'Projapoti'}
+                  ])
+
+Book.create!(title: 'Gnuplot in Action', isbn: '978-1-933-98839-9', page_count: 360, price: 66.37,
+             description: 'A book on awesome opensource plotting sofeware', published_at: '2010-11-04',
+             publisher_id:4,
+             book_cover: File.open(File.join(Rails.root, 'app/assets/images/gnupknot.jpg')))
