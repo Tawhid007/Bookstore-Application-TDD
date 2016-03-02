c8ac606750316621166ce061057056bb1c6d2883
tawhid <tawhid@nascenia.com>
2016-03-02 14:30:19 +0600

    Create sign in view template

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "catalogs#index"
 
   get '/signin', to: 'sessions#new', as: 'signin'
+  get '/signup', to: 'users#new', as: 'signup'
   post '/signin', to: 'sessions#create'
 
   resources :authors, :publishers, :books
